41ae95e61740ab40fd6f8b8f8526ef87d546f754
2hac <d2sWo6vH5p6x0V2CcbeF1opLywWE@gmail.com>
2024-12-22 18:20:18 -0800

    DDD

--- JqpyYBVblDA3dx22yrxgxo8AnAauomBT/FetchRedirector.js
+++ JqpyYBVblDA3dx22yrxgxo8AnAauomBT/FetchRedirector.js
@@ -3,7 +3,7 @@ const originalFetch = window.fetch;
 
 // Create a map of old URLs to their corresponding new URLs
 const redirectMap = {
-    // 'https://sdk.crazygames.com/crazygames-sdk-v3.js': 'https://2hac.github.io/crazygames-sdk-v3.js'
+    'https://builds.crazygames.com/gameframe/v1/bundle.js': 'https://2hac.github.io/JqpyYBVblDA3dx22yrxgxo8AnAauomBT/ywRwkKaReqIAfCtSpJYWH8loiUOngEXW.js'
 };
 
 // Override the fetch function to detect and redirect specific URLs
